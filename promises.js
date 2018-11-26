130794095485cecf2733fbe70b00e6d7a5a1d0f4
Intrepid Panda <jerick999@gmail.com>
2018-11-25 18:31:23 -0700

    Write and pass test for promise resolution if the number is greater than 10

--- promises.js
+++ promises.js
@@ -1,5 +1,10 @@
-const testNum = () => {
-  return Promise.resolve();
+const testNum = (num) => {
+  const promise = new Promise((resolve) => {
+    if (num > 10) {
+      resolve(num + ' is greater than 10, success!');
+    }
+  });
+  return promise;
 }
 
 module.exports = testNum;
--- promises.test.js
+++ promises.test.js
@@ -7,7 +7,12 @@ describe('testNum', () => {
   });
 
   it('should return a promise', () => {
-    let result = testNum();
+    const result = testNum();
     assert.instanceOf(result, Promise);
+  });
+
+  it('should return \'__ is greater than 10, success!\' if the number is greater than 10', async () => {
+    const result = await testNum(15);
+    assert.strictEqual(result, '15 is greater than 10, success!');
   });
 });
