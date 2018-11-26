77dec2a9a7bd674c122d4685fb5df2b9b6046eb9
Intrepid Panda <jerick999@gmail.com>
2018-11-25 19:01:15 -0700

    Rewrite tests with .catch since node is depricating unhandled promise rejections

--- promises.test.js
+++ promises.test.js
@@ -6,21 +6,23 @@ describe('testNum', () => {
     assert.isFunction(testNum);
   });
 
-  it('should return a promise', () => {
-    const result = testNum();
-    assert.instanceOf(result, Promise);
+  it('should return \'__ is greater than 10, success!\' if the number is greater than 10', () => {
+    return testNum(15)
+      .then(result => {
+        assert.strictEqual(result, '15 is greater than 10, success!');
+      })
+      .catch(error => {
+        throw new Error('Entering 15 should not throw an error.');
+      });
   });
 
-  it('should return \'__ is greater than 10, success!\' if the number is greater than 10', async () => {
-    const result = await testNum(15);
-    assert.strictEqual(result, '15 is greater than 10, success!');
-  });
-
-  it('should return \'__ is less than 10, error!\' if the number is less than 10', async () => {
-    try {
-      testNum(5);
-    } catch (error) {
-      assert.strictEqual(error, '5 is less than 10, error!');
-    }
+  it('should return \'__ is less than 10, error!\' if the number is less than 10', () => {
+    return testNum(5)
+      .then(result => {
+        throw new Error('Entering 5 should throw the other error.');
+      })
+      .catch(error => {
+        assert.strictEqual(error, '5 is less than 10, error!')
+      });
   });
 });
